c1dedf5bb3f57910c920fbe9e09eaae389c1ca27
samiam2007 <samanthastultz@gmail.com>
2025-08-27 16:32:48 -0500

    Add shimmer_loading.dart

---
+++ lib/widgets/shimmer_loading.dart
@@ -0,0 +1,226 @@
+import 'package:flutter/material.dart';
+
+class ShimmerLoading extends StatefulWidget {
+  final Widget child;
+  final bool isLoading;
+  final Duration duration;
+  final Color? baseColor;
+  final Color? highlightColor;
+
+  const ShimmerLoading({
+    Key? key,
+    required this.child,
+    this.isLoading = true,
+    this.duration = const Duration(milliseconds: 1500),
+    this.baseColor,
+    this.highlightColor,
+  }) : super(key: key);
+
+  @override
+  State<ShimmerLoading> createState() => _ShimmerLoadingState();
+}
+
+class _ShimmerLoadingState extends State<ShimmerLoading>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: widget.duration,
+      vsync: this,
+    );
+    _animation = Tween<double>(
+      begin: -2,
+      end: 2,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOutSine,
+    ));
+    
+    if (widget.isLoading) {
+      _animationController.repeat();
+    }
+  }
+
+  @override
+  void didUpdateWidget(ShimmerLoading oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.isLoading) {
+      _animationController.repeat();
+    } else {
+      _animationController.stop();
+    }
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!widget.isLoading) {
+      return widget.child;
+    }
+
+    final theme = Theme.of(context);
+    final isDark = theme.brightness == Brightness.dark;
+    final baseColor = widget.baseColor ?? 
+        (isDark ? Colors.grey[800]! : Colors.grey[300]!);
+    final highlightColor = widget.highlightColor ?? 
+        (isDark ? Colors.grey[700]! : Colors.grey[100]!);
+
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        return ShaderMask(
+          blendMode: BlendMode.srcIn,
+          shaderCallback: (bounds) {
+            return LinearGradient(
+              colors: [
+                baseColor,
+                highlightColor,
+                baseColor,
+              ],
+              stops: const [0.0, 0.5, 1.0],
+              begin: Alignment(_animation.value - 1, 0),
+              end: Alignment(_animation.value + 1, 0),
+            ).createShader(bounds);
+          },
+          child: widget.child,
+        );
+      },
+    );
+  }
+}
+
+class ShimmerCard extends StatelessWidget {
+  final double? width;
+  final double? height;
+  final double borderRadius;
+
+  const ShimmerCard({
+    Key? key,
+    this.width,
+    this.height = 200,
+    this.borderRadius = 16,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ShimmerLoading(
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(borderRadius),
+        ),
+      ),
+    );
+  }
+}
+
+class ShimmerText extends StatelessWidget {
+  final double width;
+  final double height;
+  final double borderRadius;
+
+  const ShimmerText({
+    Key? key,
+    required this.width,
+    this.height = 16,
+    this.borderRadius = 4,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ShimmerLoading(
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(borderRadius),
+        ),
+      ),
+    );
+  }
+}
+
+class ShimmerList extends StatelessWidget {
+  final int itemCount;
+  final double itemHeight;
+  final double spacing;
+
+  const ShimmerList({
+    Key? key,
+    this.itemCount = 5,
+    this.itemHeight = 80,
+    this.spacing = 16,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: itemCount,
+      separatorBuilder: (context, index) => SizedBox(height: spacing),
+      itemBuilder: (context, index) {
+        return ShimmerLoading(
+          child: Container(
+            height: itemHeight,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              children: [
+                Container(
+                  width: 48,
+                  height: 48,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 16,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[300],
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Container(
+                        height: 12,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[300],
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
